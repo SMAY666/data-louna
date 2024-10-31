e2960ee91ead748c77f1605d650e671a80e40fe9
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-10-31 12:03:36 +0700

    feat(environments): add env validation

---
+++ src/constants/env.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config.js'
+import {cleanEnv, num, str} from 'envalid';
+
+export const ENV = cleanEnv(
+    process.env, {
+        HOST: str(),
+        PORT: num(),
+        DB_HOST: num(),
+        DB_PORT: num(),
+        DB_NAME: str(),
+        DB_USER: str(),
+        DB_PASSWORD: str(),
+    });
