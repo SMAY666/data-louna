924a886c484828db27caa93f27df43edbe72a844
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-11-01 16:06:18 +0700

    feat(users): add schema for change_password route

--- src/routes/v1/user/index.ts
+++ src/routes/v1/user/index.ts
@@ -4,7 +4,45 @@ import {controller} from './controller';
 export const usersRoute: FastifyPluginCallback = (instance, opts, done) => {
     instance.post(
         '/change_password',
-        {},
+        {
+            // @ts-ignore
+            onRequest: instance.verifySession,
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['oldPassword', 'newPassword'],
+                    properties: {
+                        oldPassword: {
+                            type: 'string',
+                            minLength: 1,
+                        },
+                        newPassword: {
+                            type: 'string',
+                            minLength: 1,
+                        },
+                    },
+                },
+                response: {
+                    200: {
+                        type: 'object',
+                        properties: {
+                            id: {
+                                $ref: 'id',
+                            },
+                            username: {
+                                type: 'string',
+                            },
+                            passwordHash: {
+                                type: 'string',
+                            },
+                            balance: {
+                                type: 'number',
+                            }
+                        }
+                    }
+                }
+            }
+        },
         controller.changePassword
     )
     done();
